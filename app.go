4cdafb5694b332bda8b0ac8e8798fb94be8a44ea
Jonas Gao <gaopinsong@live.cn>
2023-10-17 17:37:20 +0800

    feat: Respond cmd result, and port host config

--- app.go
+++ app.go
@@ -8,15 +8,25 @@ import (
 	"strings"
 )
 
-func callShell(script string, w http.ResponseWriter) {
+func write(w http.ResponseWriter, content string, title string) {
+	_, err := w.Write([]byte(content))
+	if err != nil {
+		log.Println("Write response: ", title, " error: ", err)
+	}
+}
+
+func run(script string, w http.ResponseWriter) {
 	cmd := exec.Command("./" + script)
 	cmd.Stdout = w
 	w.WriteHeader(200)
 	err := cmd.Run()
+	write(w, "\n\n--------------------\n", "[split line]")
 	if err != nil {
 		log.Println("Run cmd return error: ", err)
+		write(w, "Run cmd fail.", "[cmd result]")
 	} else {
 		log.Println("Call \"" + script + "\" success")
+		write(w, "Run cmd success.", "[cmd result]")
 	}
 }
 
@@ -28,18 +38,32 @@ func handle(w http.ResponseWriter, req *http.Request) {
 		log.Println("Not found: " + scriptName)
 		return
 	}
-	callShell(scriptName, w)
+	run(scriptName, w)
 }
 
 func main() {
+	// Get and print cwd
 	cwd, err := os.Getwd()
 	if err != nil {
 		log.Fatal("Can not get current work directory.")
 	}
 	log.Println("Search script file in \"" + cwd + "\"")
+	// Setup handle
 	http.HandleFunc("/", handle)
-	log.Println("Listen for :8090")
-	err = http.ListenAndServe(":8090", nil)
+	// Get listen for http port
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		// Setup default port
+		port = "8090"
+	}
+	// Get listen for http host
+	host, set := os.LookupEnv("HTTP_HOST")
+	if !set {
+		host = "127.0.0.1"
+	}
+	addr := host + ":" + port
+	log.Println("Listen for ", addr)
+	err = http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
